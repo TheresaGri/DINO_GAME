987b74aa8d44caf0e46380b20bf361104019a994
Theresa Grimus <T.Grimus@gmx.net>
2022-12-03 15:16:47 +0100

    .

--- dino-game/index.js
+++ dino-game/index.js
@@ -3,30 +3,33 @@ const WIDTH = 800;
 const HEIGHT = 256;
 
 let DINO_EL = document.querySelector("#dino");
+let body = document.querySelector("body");
+
+function timer() {
+  setInterval(update,16.67);
+ }
 
 function main() {
-  let time = 16.67;
+   body.addEventListener("keydown", ()=> {
+    timer();
+    if (bottom < 10) {
+      clearInterval(timer);
+      }
 
-  setInterval(update, time);
-  //console.log(DINO_EL.style.marginTop);
-
+  });
+  
+  
 }
+let bottom = 0;
+let velocity = 20;
 
-let margin = 0;
-let counter = 0;
 
 function update() {
-//free fall equation: h = h0 + (1/2) * gravity *(counter* time)^2; 
- let gravity = 20;
- counter++;
- let tau = counter * 16.67 * Math.pow(10,-3);
- margin = 0.5 * gravity * Math.pow(tau, 2)
- DINO_EL.style.marginTop  = margin + "px";
-
-  let topMargin = 256;
-  if (margin > 200) {
-    DINO_EL.style.marginTop = (topMargin - 64) + "px";
-  }
+  velocity -= 1;
+  bottom += velocity;
+  DINO_EL.style.bottom = bottom + "px";
+  console.log(velocity);
+ 
 }
 
 main();
